0426a1e9c4df59c963da6cfc0486f975d8ba1add
らく <katz03010@gmail.com>
2022-06-25 15:55:11 +0900

    ポータルカメラ追加

--- Assets/Chan/Portal.cs
+++ Assets/Chan/Portal.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
 
-    [SerializeField]public Transform portalOut;   
+    [SerializeField]public Transform portalOut; 
 
     private void OnTriggerEnter(Collider other)
     {
-        transform.position = portalOut.position + new Vector3(1,0,0);//プレイヤーを出口に転送する
-        
+        //transform.position = portalOut.position + new Vector3(1,0,0);//プレイヤーを出口に転送する
+        transform.SetPositionAndRotation(portalOut.position + new Vector3(1,0,0), Quaternion.FromToRotation(portalOut.position, new Vector3(0,0,-1)));
     }
 
 }
